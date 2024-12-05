2cc2423708e231d70176718db6bee51208344234
ERMecca <mecca.e@northeastern.edu>
2024-12-05 14:33:48 -0500

    initial testing

---
+++ api/backend/admins/admin_routes.py
@@ -0,0 +1,17 @@
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from flask import current_app
+from backend.db_connection import db
+
+admins = Blueprint('admins', __name__)
+
+@admins.route('/system_admins', methods=['GET'])
+def get_admins():
+    cursor = db.get_db().cursor()
+    cursor.execute('''SELECT firstName, lastName FROM system_admins''')
+    theData = cursor.fetchall()
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+    return the_response
---
+++ app/src/pages/03_admin_home.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from modules.nav import SideBarLinks
+import requests
+import logging
+
+logger = logging.getLogger(__name__)
+
+st.set_page_config(layout = 'wide')
+
+# Sidebar links
+SideBarLinks()
+
+st.title('System Admin Home Page')
+st.write('')
+st.write('')
+st.write('### Menu:')
+
+if st.button('System Analytics', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/03_system_analytics.py')
+
+if st.button('System Dashboard',
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/03_system_dashboard.py')
