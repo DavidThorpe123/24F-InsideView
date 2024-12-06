d53b4acd9d9cf6a339ed3dd4701a760a24abed26
ERMecca <mecca.e@northeastern.edu>
2024-12-06 16:10:19 -0500

    Update student_routes.py

--- api/backend/students/student_routes.py
+++ api/backend/students/student_routes.py
@@ -40,3 +40,39 @@ def get_students():
     the_response.status_code = 200
     return the_response
 
+@students.route('/students/<stu_id>', methods=['PUT'])
+def update_student_last_name(stu_id):
+    cursor = db.get_db().cursor()
+    last_name = request.json['new_last_name']
+    cursor.execute('''UPDATE students
+                   SET lastName = %s
+                   WHERE id = %s''',
+                   last_name, stu_id)
+    db.get_db().commit()
+    the_response = make_response(jsonify('Student last name updated'))
+    the_response.status_code = 200
+    return the_response
+
+@students.route('/students', methods=['POST'])
+def create_student():
+    student_data = request.json()
+    if not all([student_data.get('advisorId'), student_data.get('collegeId')]):
+        return jsonify({'error': 'Advisor ID and College ID are required'}), 400
+    advisor_id = student_data['advisorId']
+    college_id = student_data['collegeId']
+    cursor = db.get_db().cursor()
+    cursor.execute("INSERT INTO students (advisorId, collegeId) VALUES (%s, %s)",
+                   (advisor_id, college_id))
+    db.get_db().commit()
+    the_response = make_response(jsonify('Student created successfully'))
+    the_response.status_code = 200
+    return the_response
+
+@students.route('/students/<stu_id>', methods=['DELETE'])
+def kill_student(stu_id):
+    cursor = db.get_db().cursor()
+    cursor.execute('''DELETE FROM students WHERE id = %s''', (stu_id))
+    db.get_db().commit()
+    the_response = make_response(jsonify('Student killed'))
+    the_response.status_code = 200
+    return the_response
