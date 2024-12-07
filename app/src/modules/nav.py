c3363123cfc9dd5edcb6c624bcedaf9994a27cf9
Alexander <70789477+AlexSchinkmannB@users.noreply.github.com>
2024-12-06 19:31:25 -0500

    Fixed minor error

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -50,10 +50,8 @@ def ClassificationNav():
 
 #### ------------------------ System Admin Role ------------------------
 def AdminPageNav():
-    st.sidebar.page_link("pages/20_Admin_Home.py", label="System Admin", icon="🖥️")
-    st.sidebar.page_link(
-        "pages/21_ML_Model_Mgmt.py", label="ML Model Management", icon="🏢"
-    )
+    st.sidebar.page_link("pages/03_admin_home.py", label="System Admin", icon="🖥️")
+
 
 
 # --------------------------------Links Function -----------------------------------------------
